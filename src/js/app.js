b2185dd30085838ca54acf364be9107c6c955b57
suizoperuano <andrechedel@hotmail.com>
2023-02-04 12:00:47 +0000

    yeahlhhhhhhh

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ import "../style/index.css";
     {
         includeCover: true, // if includeCover is true the algorithm should
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+        avatarURL: "https://scontent.fzrh3-1.fna.fbcdn.net/v/t1.6435-9/67030873_10217899780838457_7073962065957748736_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=0mVy8HKs5fAAX_Cpb94&_nc_ht=scontent.fzrh3-1.fna&oh=00_AfDiwa1qO-sXeTq6xyJfYJ6PQUjjvc3LTWr1p49enmqsTA&oe=6405966A", // this is the url for the profile avatar
         socialMediaPosition: "left", // social media bar position (left or right)
         
         twitter: null, // social media usernames
@@ -29,18 +29,36 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  function checkIfNull(MyVariable) {
+    if (MyVariable == null) {
+      return "";
+    }
+    return MyVariable;
+  }
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${checkIfNull(variables.name) +
+            " " +
+            checkIfNull(variables.lastname)}</h1>
+          <h2>${checkIfNull(variables.role)}</h2>
+          <h3>${checkIfNull(variables.city) + ","} ${checkIfNull(
+    variables.country
+  )}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +72,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://thumbs.dreamstime.com/b/steampunk-background-machine-mechanical-parts-large-gears-chains-machines-tractors-steampunk-background-machine-130985919.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://scontent.fzrh3-1.fna.fbcdn.net/v/t1.6435-9/67030873_10217899780838457_7073962065957748736_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=0mVy8HKs5fAAX_Cpb94&_nc_ht=scontent.fzrh3-1.fna&oh=00_AfDiwa1qO-sXeTq6xyJfYJ6PQUjjvc3LTWr1p49enmqsTA&oe=6405966A",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
